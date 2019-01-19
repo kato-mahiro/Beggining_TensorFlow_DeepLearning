e981d919c6fd6764b3747a4953d9389c269d00b2
katomasahiro10 <katomasahiro10@gmail.com>
2019-01-19 15:05:30 +0900

    create reduce_mean.py
    a tutorial of tf.reduce_mean()

---
+++ reduce_mean.py
@@ -0,0 +1,20 @@
+#coding:utf-8
+"""
+tf.reduce_mean()はテンソルの平均値を返すオペレーション
+第一引数は平均を取る対象のテンソル
+第二引数はどの軸方向に平均を取るか指定する
+軸を指定しない場合にはテンソルの全要素の平均を取る
+"""
+import tensorflow as tf
+a = [[1.,2.,3.],[4.,5.,6.]]
+
+x = tf.placeholder(tf.float32,[2, 3])
+y_1 = tf.reduce_mean(x, axis=0)
+y_2 = tf.reduce_mean(x, axis=1)
+y_3 = tf.reduce_mean(x)
+
+with tf.Session() as sess:
+    print(a)
+    print(sess.run(y_1,feed_dict={x:a}))
+    print(sess.run(y_2,feed_dict={x:a}))
+    print(sess.run(y_3,feed_dict={x:a}))
